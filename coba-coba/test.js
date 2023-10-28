9e8a86a9c864a703d7199c2cc046d207995c2763
faisalyudiansah <mfaisal.yudiansah@gmail.com>
2023-10-28 23:56:02 +0700

    upload

---
+++ coba-coba/test.js
@@ -0,0 +1,19 @@
+// function hitung(){
+//     return console.log('berhasil')
+// }
+// hitung()
+
+// let hitung = function (){
+//     return console.log('berhasil')
+// }
+// hitung()
+
+//============================================== IIFE
+
+// let hitung = (function (){
+//     return console.log('berhasil')
+// })()
+
+(function hitung (){
+    return console.log('berhasil')
+})()
